14ba6426fba5885e6f8b58c8cf40e2884272f909
3mawy <m.3mawyy@gmail.com>
2022-04-21 16:08:01 +0200

    initial commit

---
+++ database/migrations/2019_08_16_000000_create_providers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvidersTable extends Migration
+{
+    // TODO: check user_id foreignKey
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('provider');
+            $table->string('provider_id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('avatar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('providers');
+    }
+}
