16ea0546a38a95a031e930b9819e0b0e513d9b5b
3mawy <m.3mawyy@gmail.com>
2022-05-22 20:57:17 +0200

    added migration files

--- database/migrations/2021_08_03_000011_create_orders_table.php
+++ database/migrations/2021_08_03_000011_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGenreTrackTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,14 @@ class CreateGenreTrackTable extends Migration
      */
     public function up()
     {
-        Schema::create('genre_track', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('track_id')
+            $table->foreignId('user_id')
                 ->unsigned()
                 ->constrained();
-            $table->foreignId('genre_id')
+            $table->foreignId('status_id')
                 ->unsigned()
-                ->constrained()
-                ->onDelete('cascade');
-
+                ->constrained();
             $table->timestamps();
         });
     }
@@ -34,6 +32,6 @@ class CreateGenreTrackTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('genre_track');
+        Schema::dropIfExists('orders');
     }
 }
--- database/migrations/2022_05_22_180053_create_reposts_table.php
+++ database/migrations/2022_05_22_180053_create_reposts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGenreTrackTable extends Migration
+class CreateRepostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,8 @@ class CreateGenreTrackTable extends Migration
      */
     public function up()
     {
-        Schema::create('genre_track', function (Blueprint $table) {
+        Schema::create('reposts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('track_id')
-                ->unsigned()
-                ->constrained();
-            $table->foreignId('genre_id')
-                ->unsigned()
-                ->constrained()
-                ->onDelete('cascade');
-
             $table->timestamps();
         });
     }
@@ -34,6 +26,6 @@ class CreateGenreTrackTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('genre_track');
+        Schema::dropIfExists('reposts');
     }
 }
